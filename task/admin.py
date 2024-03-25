be103f459fde7c22c2e9bb1d24214a9e9cb5fe0c
sergei <you@example.com>
2024-03-25 21:11:15 +0800

    add models

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from task.models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'tags',
+                    'count_solutions',
+                    'name',
+                    'numbers',
+                    'index',
+                    'complexity',
+                    'number_contest',
+                    )
+
